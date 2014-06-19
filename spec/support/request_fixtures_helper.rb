b754b928f360a65fc2558c84888d18f25deae94a
Claudio Poli <masterkain@gmail.com>
2014-06-19 19:12:22 +0200

    remove editor template default comments

--- spec/support/request_fixtures_helper.rb
+++ spec/support/request_fixtures_helper.rb
@@ -1,13 +1,3 @@
-#--
-# Copyright (c) 2009-2014 iCoreTech, Inc.
-#
-# This file is part of the AudioBox.fm project.
-#++
-
-# Author::    Claudio Poli (mailto:claudio@icorete.ch)
-# Copyright:: Copyright (c) 2009-2014 iCoreTech, Inc.
-# License::   iCoreTech, Inc. Private License
-
 module RequestFixturesHelper
   def request_fixture(name)
     File.read("spec/fixtures/#{name}.json")
