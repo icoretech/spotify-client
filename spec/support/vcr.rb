6ad3f9d1b030f4b41c31c7651f5dc2e512b31de0
Claudio Poli <masterkain@gmail.com>
2014-06-19 05:10:59 +0200

    don't require vcr yet

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -1,9 +1,9 @@
 # VCR configuration file.
-require 'vcr'
-
-VCR.configure do |c|
-  c.configure_rspec_metadata!
-  c.hook_into :excon
-  c.cassette_library_dir = 'spec/fixtures'
-  c.default_cassette_options = { record: :none }
-end
+# require 'vcr'
+#
+# VCR.configure do |c|
+#   c.configure_rspec_metadata!
+#   c.hook_into :excon
+#   c.cassette_library_dir = 'spec/fixtures'
+#   c.default_cassette_options = { record: :none }
+# end
